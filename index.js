13d3f9b599ab46dba77202047b625b135b2babce
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-13 07:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(holiday, days) {
+function holidayCountdown(holiday, ) {
   return `It's ${
 }
